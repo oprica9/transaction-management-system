777ba08d89863b18ac7c422fe42cabfd468b7f4d
Ognjen Prica <ognjen.prica9@gmail.com>
2026-07-08 19:28:19 +0200

    Initial commit, Spring Boot setup

---
+++ tms-api/src/main/java/com/oprica/tmsapi/TmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.oprica.tmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmsApiApplication.class, args);
+    }
+
+}
---
+++ tms-api/src/test/java/com/oprica/tmsapi/TmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oprica.tmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
